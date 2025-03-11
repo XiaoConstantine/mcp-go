553ae1d948bb7d7f866a845637ec2cf7e0ab95ea
Xiao Cui <constantine124@gmail.com>
2025-03-10 21:56:06 -0400

    fix lint

--- pkg/server/logging/adapter.go
+++ pkg/server/logging/adapter.go
@@ -7,13 +7,13 @@ import (
 	"github.com/XiaoConstantine/mcp-go/pkg/model"
 )
 
-// ManagerAdapter adapts a Manager to implement the logging.Logger interface
+// ManagerAdapter adapts a Manager to implement the logging.Logger interface.
 type ManagerAdapter struct {
 	manager *Manager
 	logger  string // Component/module name
 }
 
-// NewManagerAdapter creates a new adapter that wraps a Manager and implements logging.Logger
+// NewManagerAdapter creates a new adapter that wraps a Manager and implements logging.Logger.
 func NewManagerAdapter(manager *Manager, loggerName string) logging.Logger {
 	return &ManagerAdapter{
 		manager: manager,
@@ -21,27 +21,27 @@ func NewManagerAdapter(manager *Manager, loggerName string) logging.Logger {
 	}
 }
 
-// Debug logs a debug message
+// Debug logs a debug message.
 func (a *ManagerAdapter) Debug(msg string, keysAndValues ...interface{}) {
 	a.manager.Log(models.LogLevelDebug, formatLogMessage(msg, keysAndValues...), a.logger)
 }
 
-// Info logs an info message
+// Info logs an info message.
 func (a *ManagerAdapter) Info(msg string, keysAndValues ...interface{}) {
 	a.manager.Log(models.LogLevelInfo, formatLogMessage(msg, keysAndValues...), a.logger)
 }
 
-// Warn logs a warning message
+// Warn logs a warning message.
 func (a *ManagerAdapter) Warn(msg string, keysAndValues ...interface{}) {
 	a.manager.Log(models.LogLevelWarning, formatLogMessage(msg, keysAndValues...), a.logger)
 }
 
-// Error logs an error message
+// Error logs an error message.
 func (a *ManagerAdapter) Error(msg string, keysAndValues ...interface{}) {
 	a.manager.Log(models.LogLevelError, formatLogMessage(msg, keysAndValues...), a.logger)
 }
 
-// formatLogMessage formats a log message with key-value pairs
+// formatLogMessage formats a log message with key-value pairs.
 func formatLogMessage(msg string, keysAndValues ...interface{}) string {
 	if len(keysAndValues) == 0 {
 		return msg
